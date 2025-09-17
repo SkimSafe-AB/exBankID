cdfdb08b080d3cc68e49f304ddf42530c3975d60
Philip Mannheimer <philip.mannheimer@gmail.com>
2025-09-17 17:11:13 +0200

    updated nimble options

--- mix.exs
+++ mix.exs
@@ -1,7 +1,7 @@
 defmodule ExBankID.MixProject do
   use Mix.Project
 
-  @version "0.2.2"
+  @version "0.2.3"
 
   def project do
     [
@@ -42,7 +42,7 @@ defmodule ExBankID.MixProject do
       {:bypass, "~> 2.0", only: :test},
       {:excoveralls, "~> 0.10", only: :test},
       {:ex_doc, ">= 0.0.0", only: :dev, runtime: false},
-      {:nimble_options, "~> 0.3.0"}
+      {:nimble_options, "~> 1.0.0"}
     ]
   end
 
